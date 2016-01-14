31531033474a1a61d69a10ffc571032e437b3331
Atvaark <Atvaark@users.noreply.github.com>
2016-01-14 23:47:37 +0100

    Implemented naming vtables in inlined constructors

--- scripts/rename_type_factories.py
+++ scripts/rename_type_factories.py
@@ -1,10 +1,47 @@
 #---------------------------------------------------------------------
 # Rename type factories
 #
-# Adds labels to the type factory vtable locations.
-#
+# * Adds labels to the type factory vtable locations.
+# * Adds labels to vtables when using inline constructors
+# * Doesn't work with constructors that the code calls or jumps to
 #---------------------------------------------------------------------
 from idautils import *
+
+def handleInlineConstructor(ea,name):
+	#print "%X" % R4
+	E = list(FuncItems(ea))
+	hit = -1
+	reg = ["eax"]
+	opnd = ["dword ptr [eax]"]
+	movRegCount = 0
+	movCount = 0
+	callCount = 0
+	for e in E:	
+		# start logging after the 2nd call.
+		if callCount < 2:
+			if GetMnem(e) == "call":
+				callCount += 1
+			continue		
+					
+		if GetMnem(e) == "mov" and GetOpType(e,0) == 3 and GetOpType(e,1) == 5 and GetOpnd(e,0) in opnd:
+			hit = e
+			movCount += 1
+			#print "%X : %s %s %s %s %s %s" % (e,GetOpType(e,0),GetOpType(e,1),GetOpnd(e,0),GetOpnd(e,1),GetOperandValue(e,0), GetOperandValue(e,1))
+			break
+		elif GetMnem(e) == "mov" and GetOpType(e,0) == 1 and GetOpType(e,1) == 1 and GetOpnd(e,1) in reg:
+			newReg = GetOpnd(e,0)
+			#print "%s -> %s" % (reg, newReg)		
+			reg.append(newReg)
+			opnd.append("dword ptr [%s]" % newReg)
+			movRegCount += 1
+			#print "%X : %s %s %s %s %s %s" % (e,GetOpType(e,0),GetOpType(e,1),GetOpnd(e,0),GetOpnd(e,1),GetOperandValue(e,0), GetOperandValue(e,1))
+			
+	if hit == -1:
+		print "%X miss %i %i %s" % (ea, movCount, movRegCount, opnd)			
+	else:
+		hitVtable = GetOperandValue(hit, 1)
+		idc.MakeName(hitVtable, "%s_vtable_0" % name)		
+		print "%X hit %X %s %X" % (ea, hit, GetDisasm(hit), hitVtable)
 
 def main():
 	tracedfunc = idc.FindBinary(0, 1, "8B 44 24 04 56 8B F1 8B 56 18 8B 4C 24 10 81 E1 ?? ?? ?? ?? 81 E2 ?? ?? ?? ??")
@@ -14,7 +51,6 @@ def main():
 		return
 	
 	#print "found at %X" % tracedfunc
-
 	for ref in CodeRefsTo(tracedfunc, 1):
 		#print "  called from %s(0x%x)" % (GetFunctionName(ref), ref)
 
@@ -52,9 +88,20 @@ def main():
 		# A5 = Size
 		# A6 = ?
 		# A7 = ?
-		Name = A2.replace("<","_").replace(">","_").replace(" ", "")		
-		idc.MakeName(A1, "p_%s_factory" % Name)
-		idc.MakeName(R2, "%s_factory" % Name)
-
+		name = A2.replace("<","_").replace(">","_").replace(" ", "")		
+		idc.MakeName(A1, "p_%s_factory" % name)
+		idc.MakeName(R2, "%s_factory" % name)
+		
+		R3 = NextHead(R2, -1)
+		if R3 == idaapi.BADADDR:
+			#print "error: create function not found"
+			continue			
+		R4 = Dword(R3)	
+		
+		if not idaapi.get_func(R4):		
+			#print "error: not a function %X" % R4
+			continue		
+		idc.MakeName(R4, "create_%s" % name)		
+		handleInlineConstructor(R4,name)	
 if __name__ == "__main__":
   main()
